fbf2079c2bb3991300a8520b270436c7753b0405
pushpakrawat <pushpak.rawat@outlook.com>
2023-05-12 11:04:36 +0530

    states and props done

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+
+    const title = "New Blogs"
+
+    return (
         <div className="home">
-            <h2>Homepage</h2>
+            <BlogList blogs={blogs} title={title}/>
+
         </div>
-     );
+    );
 }
- 
+
 export default Home;

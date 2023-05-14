7a99600320f2bd6aac4cb323d09b5858e27015d7
pushpakrawat <pushpak.rawat@outlook.com>
2023-05-14 13:47:25 +0530

    learnt creating custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
+    const { error, data : blogs,  isPending} = useFetch('http://localhost:8000/blogs')
 
-    // const handleDelete = (id)=> {
-    //     const newBlogs = blogs.filter( (blog) => blog.id !== id );
-    //     setBlogs(newBlogs);
-    // }
-
-    useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then( data =>{
-                setBlogs(data);
-                setIsPending(false);
-            });
-    }, [])
+    
 
     
 
     return (
         <div className="home">
+            { error && <div> {error} </div>}
             { isPending && <div>Loading...</div> } 
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
             

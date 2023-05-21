c89ce65acb0e21661a13f478880f237a8af35462
pushpakrawat <pushpak.rawat@outlook.com>
2023-05-21 13:45:55 +0530

    leant about using states in form and also updating form when change occurs | Can be used in maing forms in my own project

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
     return (
       <div className="create">
-        <h2>Add a New Blog</h2>
+        <h1>Add a new blog</h1>
+        <form>
+          <label> Blog Title: </label>
+          <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+          <label> Blog Body:</label>
+          <textarea value={body} onChange={(e)=>{setBody(e.target.value)}}></textarea>
+          <label>Written by :</label>
+          <select value={author} onChange={(e)=>{setAuthor(e.target.value)}}>
+            <option value="Mario">Mario</option>
+            <option value="Yoshi">Yoshi</option>
+          </select>
+          <button>Submit</button>
+        </form>
+        <p>{title}</p>
+        <p>{body}</p>
       </div>
     );
   }
